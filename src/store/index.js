5abfad5be3b8174a80b6ba139a21604ab5d2901e
Fred Guyton <fguyton@gmail.com>
2021-10-03 08:57:39 -0400

    url fix

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,8 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    base_urlx: "https://polar-atoll-48970.herokuapp.com",
-    base_url: "http://127.0.0.1:5000/"
+    base_url: "https://polar-atoll-48970.herokuapp.com",
+    base_urlx: "http://127.0.0.1:5000/"
   },
   mutations: {
   },
